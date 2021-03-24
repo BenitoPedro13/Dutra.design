fdd1f0422c3d5aca541b8ffa98e62be3910f20c6
Benito Pedro Xavier Neto <benitopedro21@outlook.com>
2021-03-24 09:59:35 -0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-import initAnimar from './anime.js';
-import SlideNav from './slide.js';
-
-const slide = new SlideNav('.slide', '.wrapper');
-slide.init();
-slide.addControl('.custom-controls');
-initAnimar();
